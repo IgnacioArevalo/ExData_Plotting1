290ca2d01bfef693c9c2c89c435eaf9926e9e3f6
Ignacio Arévalo Martín <nachoarevalomartin@gmail.com>
2015-02-07 22:50:51 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,10 +1,17 @@
+# Set your working directory to access the data file "household_power_consumption.txt" 
 setwd("C:/Users/Nacho/Desktop/Johns Hopkins Data Science/4. EXPLORATORY DATA ANALYSIS")
 library(graphics)
 library(grDevices)
+# Load the data reading only values for 01/02/2007 and 02/02/2007
+# Having a look at the file we can find where to start counting
+# The process skips 66636 rows (all of the days before 01/02/2007) 
+# Afterwards it reads 2880 rows that correspond to 01/02/2007 and 02/02/2007
 household<-read.csv2("household_power_consumption.txt",header=TRUE,skip=66636,nrows=2880,col.names=c("Date","Time","GlobalActivePower","GlobalReactivePower","Voltage","GlobalIntensity","Submetering1","Submetering2","Submetering3"))
+# Create the variables to be plotted
 Submetering1<-as.numeric(as.character(household$Submetering1))
 Submetering2<-as.numeric(as.character(household$Submetering2))
 Submetering3<-as.numeric(as.character(household$Submetering3))
+# Plot the figure as described in the project 
 png(filename="plot3.png",width=480,height=480,units="px",bg="transparent")
 plot(c(1:2880),Submetering1,type="l",xlab="",ylab="Energy sub metering",xaxp=c(1,2880,2),xaxt="n")
 lines(c(1:2880),Submetering2,type="l",col="red")
