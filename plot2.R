7d06d378dc95deac7a2e8c84d28596a313ad1738
Ignacio Arévalo Martín <nachoarevalomartin@gmail.com>
2015-02-07 12:21:07 +0100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,7 +1,7 @@
 setwd("C:/Users/Nacho/Desktop/Johns Hopkins Data Science/4. EXPLORATORY DATA ANALYSIS")
 library(graphics)
 library(grDevices)
-household<-read.csv2("household_power_consumption.txt",header=TRUE,skip=66636,nrows=2880,col.names=c("Date","Time","GlobalActivePower","GlobalReactivePower","Voltage","GlobalIntensity","Submetering1","Submetering2","Submetering3")
+household<-read.csv2("household_power_consumption.txt",header=TRUE,skip=66636,nrows=2880,col.names=c("Date","Time","GlobalActivePower","GlobalReactivePower","Voltage","GlobalIntensity","Submetering1","Submetering2","Submetering3"))
 GlobalActivePower<-as.numeric(as.character(household$GlobalActivePower))
 png(filename="plot2.png",width=480,height=480,units="px",bg="transparent")
 plot(c(1:2880),GlobalActivePower,type="l",xlab="",ylab="Global Active Power (kilowatts)",xaxp=c(1,2880,2),xaxt="n")
